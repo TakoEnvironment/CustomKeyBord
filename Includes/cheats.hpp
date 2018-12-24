f4161dd3f27966f5f082fd7409262cf571b97026
TakoEnvironment <45265257+TakoEnvironment@users.noreply.github.com>
2018-12-25 00:29:00 +0900

    Add files via upload

---
+++ Includes/cheats.hpp
@@ -0,0 +1,16 @@
+#ifndef CHEATS_H
+#define CHEATS_H
+
+#include "CTRPluginFramework.hpp"
+#include "Helpers.hpp"
+#include "Unicode.h"
+
+namespace CTRPluginFramework
+{
+	void	custom_old(MenuEntry *entry);
+	void	custom_new(MenuEntry *entry);
+	void	No_1(MenuEntry *entry);
+	void	No_2(MenuEntry *entry);
+}
+
+#endif
